4762fb4d54038226d7ecaec6b73d1de4c2ea150c
Gavin Costa <143152149+GavinCosta@users.noreply.github.com>
2023-09-26 12:43:19 -0400

    Update main.js
    Updated welcome function with an example of chaining then method to result a combined value

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
+//welcome function
+welcome()
+  .then(console.log) // Logs the result of welcome()
+  .then(goodbye) // Returns promise from goodbye()
+  .then(console.log) // Logs the result of goodbye()
+  .catch(console.error); // Logs error from welcome() or goodbye()
 
-const promise = welcome();
+//welcome function with combined then methods (nested)
+welcome()
+  .then((welcomeMessage) =>
+    goodbye().then((goodbyeMessage) => `${welcomeMessage}\n${goodbyeMessage}`)
+  ) // welcomeMessage and goodbyeMessage combined.
+  .then(console.log) // Logs combines messages
+  .catch(console.error); // Logs error from welcome() or goodbye()
 
 console.log(promise);
 
 const question = "Will the weather be nice today?";
-const tellPromise = tell(question);
-tellPromise.then((fortune) => {
+tell(question)
+  .then((fortune) => {
   console.log(question);
   console.log(fortune);
-});
+})
+  .catch(console.error);
